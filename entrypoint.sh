14d5799dd62f22d41111b64f466a4c700de994e7
Karn Wong <karn@karnwong.me>
2024-07-13 16:23:42 +0700

    add schema migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ if [ -n "$MLFLOW_S3_ENDPOINT_URL" ]; then
 	aws s3 --endpoint-url "$MLFLOW_S3_ENDPOINT_URL" cp test_file.txt "$DEFAULT_ARTIFACT_ROOT/test_file.txt" >/dev/null 2>&1
 	WRITE_PERMISSION=$?
 else
-	aws s3 ls $DEFAULT_ARTIFACT_ROOT >/dev/null 2>&1
+	aws s3 ls "$DEFAULT_ARTIFACT_ROOT" >/dev/null 2>&1
 	READ_PERMISSION=$?
 
 	aws s3 cp test_file.txt "$DEFAULT_ARTIFACT_ROOT/test_file.txt" >/dev/null 2>&1
@@ -55,11 +55,15 @@ rm test_file.txt
 
 if [ $READ_PERMISSION -eq 0 ] && [ $WRITE_PERMISSION -eq 0 ]; then
 	echo "Read and write permissions are granted for the S3 bucket '$DEFAULT_ARTIFACT_ROOT'."
+
 	################################
 	# entrypoint
 	################################
+	postgres_uri="postgresql://$POSTGRES_USERNAME:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE"
+
+	mlflow db upgrade "$postgres_uri"
 	mlflow server \
-		--backend-store-uri "postgresql://$POSTGRES_USERNAME:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DATABASE" \
+		--backend-store-uri "$postgres_uri" \
 		--artifacts-destination "$DEFAULT_ARTIFACT_ROOT" \
 		--host 0.0.0.0 \
 		--port 8080
